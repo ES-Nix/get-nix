cb554d15913ac5c93cf45cbf970d4784c966033c
Pedro Regis <pedroalencarregis@hotmail.com>
2021-01-30 02:52:52 -0300

    Start to test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # get-nix
 Is a unofficial wrapper of the nix installer.
+
+
+`curl -fsSL https://raw.githubusercontent.com/ES-Nix/get-nix//get-nix.sh | sh`
+
---
+++ get-nix.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+
+test -d /nix || sudo mkdir --mode=0755 /nix \
+&& sudo chown "$USER": /nix \
+&& command -v nix >/dev/null 2>&1 || curl -L https://nixos.org/nix/install | sh \
+&& test -d ~/.config/nix || mkdir --parent --mode=755 ~/.config/nix && touch ~/.config/nix/nix.conf \
+&& cat ~/.config/nix/nix.conf | grep 'nixos' >/dev/null && /bin/true || echo 'system-features = kvm nixos-test' >> ~/.config/nix/nix.conf \
+&& cat ~/.config/nix/nix.conf | grep 'flakes' >/dev/null && /bin/true || echo 'experimental-features = nix-command flakes ca-references' >> ~/.config/nix/nix.conf \
+&& test -d ~/.config/nixpkgs || mkdir --parent --mode=755 ~/.config/nixpkgs && touch ~/.config/nixpkgs/config.nix \
+&& cat ~/.config/nixpkgs/config.nix | grep 'allowUnfree' >/dev/null && /bin/true || echo '{ allowUnfree = true; }' >> ~/.config/nixpkgs/config.nix \
+&& cat ~/.bashrc | grep 'flake' >/dev/null && /bin/true || echo "alias flake='nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes'" >> ~/.bashrc \
+&& cat ~/.bashrc | grep 'collect' >/dev/null && /bin/true || echo "alias nd='nix-collect-garbage --delete-old'" >> ~/.bashrc \
+&& . "$HOME"/.nix-profile/etc/profile.d/nix.sh \
+&& . ~/.bashrc
