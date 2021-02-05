9f55ec4ff27ad64e2704855bfcc4bd1dd697b460
Pedro Regis <pedroalencarregis@hotmail.com>
2021-02-05 17:37:12 -0300

    Saving for remote test

--- get-nix.sh
+++ get-nix.sh
@@ -14,8 +14,41 @@ test -d /nix || sudo mkdir --mode=0755 /nix \
 && cat ~/.config/nix/nix.conf | grep 'trace' >/dev/null && /bin/true || echo 'show-trace = true' >> ~/.config/nix/nix.conf \
 && test -d ~/.config/nixpkgs || mkdir --parent --mode=755 ~/.config/nixpkgs && touch ~/.config/nixpkgs/config.nix \
 && cat ~/.config/nixpkgs/config.nix | grep 'allowUnfree' >/dev/null && /bin/true || echo '{ allowUnfree = true; }' >> ~/.config/nixpkgs/config.nix \
-&& cat ~/.bashrc | grep 'flake' >/dev/null && /bin/true || echo "alias flake='nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes'" >> ~/.bashrc \
-&& cat ~/.bashrc | grep 'collect' >/dev/null && /bin/true || echo "alias nd='nix-collect-garbage --delete-old'" >> ~/.bashrc \
-&& cat ~/.bashrc | grep 'develop' >/dev/null && /bin/true || echo "alias develop=\"nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes --run 'nix develop'\"" >> ~/.bashrc \
 && . "$HOME"/.nix-profile/etc/profile.d/nix.sh \
 && . ~/.bashrc
+
+
+#&& cat ~/.bashrc | grep 'flake' >/dev/null && /bin/true || echo "alias flake='nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes'" >> ~/.bashrc \
+#&& cat ~/.bashrc | grep 'collect' >/dev/null && /bin/true || echo "alias nd='nix-collect-garbage --delete-old'" >> ~/.bashrc \
+#&& cat ~/.bashrc | grep 'develop' >/dev/null && /bin/true || echo "alias develop=\"nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes --run 'nix develop'\"" >> ~/.bashrc \
+
+
+# Main ideia from: https://stackoverflow.com/a/1655389
+read -r -d '' PROFILE_NIX_FUNCTIONS <<-'EOF'
+    flake()
+    {
+        echo "Entering the nix + flake shell."
+        nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes "$@"
+    }
+    export -f flake
+
+    nd()
+    {
+        nix-collect-garbage --delete-old
+    }
+    export -f nd
+
+    develop()
+    {
+        echo "Entering the nix + flake development shell."
+        nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes --run 'nix develop'
+    }
+    export -f develop
+    EOF
+
+
+if [ ! -f ~/.profile ]; then
+  cat "$PROFILE_NIX_FUNCTIONS" > ~/.profile
+else
+  grep 'flake' ~/.profile --quiet || cat "$PROFILE_NIX_FUNCTIONS" >> ~/.profile
+fi
