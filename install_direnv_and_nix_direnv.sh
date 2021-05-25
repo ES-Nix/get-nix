7ef6b72ffb9c37668c6ad4d4deac0ac31788c3e6
Pedro Regis <pedroalencarregis@hotmail.com>
2021-05-25 00:06:51 -0300

    Adds direnv nix-direnv install script

---
+++ install_direnv_and_nix_direnv.sh
@@ -0,0 +1,10 @@
+nix profile install nixpkgs#direnv
+nix profile install nixpkgs#nix-direnv
+echo "source $(nix eval --raw nixpkgs#nix-direnv)"'/share/nix-direnv/direnvrc' >> ~/.direnvrc
+echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> ~/.bashrc
+echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> ~/.bashrc
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+. ~/.bashrc
+. ~/.direnvrc
+direnv --version
+
