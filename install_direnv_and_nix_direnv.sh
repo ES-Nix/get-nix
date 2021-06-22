0ff5745bac5328d7492e3c6d8023bb9a2a952bda
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-22 14:56:36 -0300

    Save to test

--- install_direnv_and_nix_direnv.sh
+++ install_direnv_and_nix_direnv.sh
@@ -1,16 +1,27 @@
+
+# One more hack...
+# https://stackoverflow.com/questions/3327013/how-to-determine-the-current-shell-im-working-on#comment83000217_11097703
+# https://stackoverflow.com/questions/3327013/how-to-determine-the-current-shell-im-working-on#comment3450154_3327027
+NIX_GUESSED_USER_SHELL=$(ps -ocomm= -q $$)
+echo 'The installer has identified the runnig shell as: '"$NIX_GUESSED_USER_SHELL"
+
+GUESSED_SHELL_RC='~/.'"$NIX_GUESSED_USER_SHELL"'rc'
+
 nix profile install nixpkgs#direnv
 nix profile install nixpkgs#nix-direnv
 echo 'source $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc' >> ~/.direnvrc
-echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> ~/.bashrc
-echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> ~/.bashrc
-echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
-. ~/.bashrc
+echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> "$GUESSED_SHELL_RC"
+echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> "$GUESSED_SHELL_RC"
+echo 'eval "$(direnv hook bash)"' >> "$GUESSED_SHELL_RC"
+. "$GUESSED_SHELL_RC"
 . ~/.direnvrc
 direnv --version
 
 nix profile install nixpkgs#gnused
 
-sudo su << COMMANDS
+sudo \
+su \
+<< COMMANDS
 sed \
 -i \
 's/NIX_BIN_PREFIX=.*/NIX_BIN_PREFIX=\"\$(nix eval --raw nixpkgs#nixFlakes)\"\/bin\//' \
