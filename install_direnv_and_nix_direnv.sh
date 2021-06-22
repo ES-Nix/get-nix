b6dfeabbe746c475f6c128ec4eb1ebfb0d375539
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-22 17:20:08 -0300

    One more try, the shell chosen was hardcoded

--- install_direnv_and_nix_direnv.sh
+++ install_direnv_and_nix_direnv.sh
@@ -14,10 +14,14 @@ echo 'Debuging the GUESSED_SHELL_RC='"$GUESSED_SHELL_RC"
 
 nix profile install nixpkgs#direnv
 nix profile install nixpkgs#nix-direnv
+
 echo 'source $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc' >> ~/.direnvrc
 echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> "$GUESSED_SHELL_RC"
 echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> "$GUESSED_SHELL_RC"
-echo 'eval "$(direnv hook bash)"' >> "$GUESSED_SHELL_RC"
+
+#
+echo 'eval "$(direnv hook '"$NIX_GUESSED_USER_SHELL"'')"' >> "$GUESSED_SHELL_RC"
+
 . "$GUESSED_SHELL_RC"
 . ~/.direnvrc
 direnv --version
