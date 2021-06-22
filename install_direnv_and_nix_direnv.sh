a234c0578e80dd83b68630124d41719990a6e600
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-22 15:07:40 -0300

    Change it again

--- install_direnv_and_nix_direnv.sh
+++ install_direnv_and_nix_direnv.sh
@@ -2,7 +2,10 @@
 # One more hack...
 # https://stackoverflow.com/questions/3327013/how-to-determine-the-current-shell-im-working-on#comment83000217_11097703
 # https://stackoverflow.com/questions/3327013/how-to-determine-the-current-shell-im-working-on#comment3450154_3327027
-NIX_GUESSED_USER_SHELL=$(ps -ocomm= -q $$)
+# NIX_GUESSED_USER_SHELL=$(ps -ocomm= -q $$)
+
+NIX_GUESSED_USER_SHELL="$(basename $(grep $USER </etc/passwd | cut -f 7 -d ":"))"
+
 echo 'The installer has identified the runnig shell as: '"$NIX_GUESSED_USER_SHELL"
 
 GUESSED_SHELL_RC='~/.'"$NIX_GUESSED_USER_SHELL"'rc'
