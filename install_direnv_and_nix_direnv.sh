426d1f203be6d2bc841ea6a05e01e198b13a8dbb
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-27 00:12:07 -0300

    Merge all lines

--- install_direnv_and_nix_direnv.sh
+++ install_direnv_and_nix_direnv.sh
@@ -4,45 +4,29 @@
 # https://stackoverflow.com/questions/3327013/how-to-determine-the-current-shell-im-working-on#comment3450154_3327027
 # NIX_GUESSED_USER_SHELL=$(ps -ocomm= -q $$)
 
-NIX_GUESSED_USER_SHELL="$(basename $(grep $USER </etc/passwd | cut -f 7 -d ":"))"
-
-echo 'The installer has identified the runnig shell as: '"$NIX_GUESSED_USER_SHELL"
-
-GUESSED_SHELL_RC=~/."$NIX_GUESSED_USER_SHELL"rc
-
-# echo 'Debuging the GUESSED_SHELL_RC='"$GUESSED_SHELL_RC"
-
-nix profile install nixpkgs#direnv
-nix profile install nixpkgs#nix-direnv
-
+NIX_GUESSED_USER_SHELL="$(basename $(grep $USER </etc/passwd | cut -f 7 -d ":"))" \
+echo 'The installer has identified the runnig shell as: '"$NIX_GUESSED_USER_SHELL" \
+GUESSED_SHELL_RC=~/."$NIX_GUESSED_USER_SHELL"rc \
+nix profile install nixpkgs#direnv \
+nix profile install nixpkgs#nix-direnv \
 echo 'source $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc' >> ~/.direnvrc \
-&& stat ~/.direnvrc
-
-# https://stackoverflow.com/a/29685539
-# sed -i '/^[^#]/ s/\(^.*plugins.*$\)/#\ \1/' "$GUESSED_SHELL_RC"
-
-echo 'export DIRENV_BASH=$(which bash)' >> "$GUESSED_SHELL_RC"
-echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> "$GUESSED_SHELL_RC"
-echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> "$GUESSED_SHELL_RC"
-echo 'eval "$(direnv hook '"$NIX_GUESSED_USER_SHELL"')"' >> "$GUESSED_SHELL_RC"
-
-sudo rm -fv /nix/store/*-nix-2.3.12/bin/nix
-
-. ~/.direnvrc
-direnv --version
-
-nix profile install nixpkgs#gnused
-
-sudo su << COMMANDS
+&& stat ~/.direnvrc \
+&& echo 'export DIRENV_BASH=$(which bash)' >> "$GUESSED_SHELL_RC" \
+&& echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> "$GUESSED_SHELL_RC" \
+&& echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> "$GUESSED_SHELL_RC" \
+&& echo 'eval "$(direnv hook '"$NIX_GUESSED_USER_SHELL"')"' >> "$GUESSED_SHELL_RC" \
+&&  sudo rm -fv /nix/store/*-nix-2.3.12/bin/nix \
+&& . ~/.direnvrc \
+&& direnv --version \
+&& nix profile install nixpkgs#gnused \
+&& sudo su << COMMANDS
 sed \
 -i \
 's/NIX_BIN_PREFIX=.*/NIX_BIN_PREFIX=\"\$(nix eval --raw nixpkgs#nixFlakes)\"\/bin\//' \
-$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc
-
-sed \
+$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc \
+&& sed \
 -i \
 '/.*NIX_BIN_PREFIX=\$(command -v nix-shell)/d' \
 $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc
-COMMANDS
-
-nix profile remove nixpkgs#gnused
+COMMANDS \
+&& nix profile remove nixpkgs#gnused
