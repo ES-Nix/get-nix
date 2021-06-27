5c11860be5eccba15e7938e9c40a7ccafbd9b90e
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-26 22:21:52 -0300

    Debuging

--- install_direnv_and_nix_direnv.sh
+++ install_direnv_and_nix_direnv.sh
@@ -14,16 +14,18 @@ echo 'Debuging the GUESSED_SHELL_RC='"$GUESSED_SHELL_RC"
 
 nix profile install nixpkgs#direnv
 nix profile install nixpkgs#nix-direnv
-
+echo 'WWWWWW'
 echo 'source $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc' >> ~/.direnvrc
 echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> "$GUESSED_SHELL_RC"
 echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> "$GUESSED_SHELL_RC"
-
+echo 'AAAAA'
 #
 echo 'eval "$(direnv hook '"$NIX_GUESSED_USER_SHELL"')"' >> "$GUESSED_SHELL_RC"
-
+echo '#####'
 . "$GUESSED_SHELL_RC"
+echo 'RRRRRRR'
 . ~/.direnvrc
+echo 'QQQQQ'
 direnv --version
 
 nix profile install nixpkgs#gnused
