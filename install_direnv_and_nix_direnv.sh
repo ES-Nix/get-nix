d284c1b214157569195ec1dab4b099a05bad60f1
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-06-27 00:03:41 -0300

    Test installer

--- install_direnv_and_nix_direnv.sh
+++ install_direnv_and_nix_direnv.sh
@@ -10,12 +10,13 @@ echo 'The installer has identified the runnig shell as: '"$NIX_GUESSED_USER_SHEL
 
 GUESSED_SHELL_RC=~/."$NIX_GUESSED_USER_SHELL"rc
 
-echo 'Debuging the GUESSED_SHELL_RC='"$GUESSED_SHELL_RC"
+# echo 'Debuging the GUESSED_SHELL_RC='"$GUESSED_SHELL_RC"
 
 nix profile install nixpkgs#direnv
 nix profile install nixpkgs#nix-direnv
-echo 'WWWWWW'
-echo 'source $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc' >> ~/.direnvrc
+
+echo 'source $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc' >> ~/.direnvrc \
+&& stat ~/.direnvrc
 
 # https://stackoverflow.com/a/29685539
 # sed -i '/^[^#]/ s/\(^.*plugins.*$\)/#\ \1/' "$GUESSED_SHELL_RC"
@@ -23,16 +24,11 @@ echo 'source $(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc' >> 
 echo 'export DIRENV_BASH=$(which bash)' >> "$GUESSED_SHELL_RC"
 echo 'export PATH=$(nix eval --raw nixpkgs#direnv)/bin:"$PATH"' >> "$GUESSED_SHELL_RC"
 echo 'export PATH=$(nix eval --raw nixpkgs#nix-direnv)/share/nix-direnv/direnvrc:"$PATH"' >> "$GUESSED_SHELL_RC"
-echo 'AAAAA' \
-&& echo 'eval "$(direnv hook '"$NIX_GUESSED_USER_SHELL"')"' >> "$GUESSED_SHELL_RC"
+echo 'eval "$(direnv hook '"$NIX_GUESSED_USER_SHELL"')"' >> "$GUESSED_SHELL_RC"
 
 sudo rm -fv /nix/store/*-nix-2.3.12/bin/nix
-# && echo '#####' \
-# && GUESSED_SHELL_RC=~/."$NIX_GUESSED_USER_SHELL"rc \
-# && . "$GUESSED_SHELL_RC" \
-# && echo 'RRRRRRR' \
+
 . ~/.direnvrc
-# echo 'QQQQQ'
 direnv --version
 
 nix profile install nixpkgs#gnused
