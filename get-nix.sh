eccef9a426fd8d7fa4c7e4a8c1191ba1cd00a4f7
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-09-01 03:38:45 -0300

    Adds system-features = benchmark big-parallel, nix shell nixpkgs#kvm
    The nix shell nixpkgs#kvm needs the big-parallel

--- get-nix.sh
+++ get-nix.sh
@@ -6,11 +6,11 @@
 #set -x
 
 command -v nix >/dev/null 2>&1 || curl -L https://nixos.org/nix/install | sh \
-&& test -d ~/.config/nix || mkdir --parent --mode=755 ~/.config/nix && touch ~/.config/nix/nix.conf \
-&& cat ~/.config/nix/nix.conf | grep 'nixos' >/dev/null && /bin/true || echo 'system-features = kvm nixos-test' >> ~/.config/nix/nix.conf \
+&& test -d ~/.config/nix || mkdir --parent --mode=0755 ~/.config/nix && touch ~/.config/nix/nix.conf \
+&& cat ~/.config/nix/nix.conf | grep 'nixos' >/dev/null && /bin/true || echo 'system-features = benchmark big-parallel kvm nixos-test' >> ~/.config/nix/nix.conf \
 && cat ~/.config/nix/nix.conf | grep 'flakes' >/dev/null && /bin/true || echo 'experimental-features = nix-command flakes ca-references ca-derivations' >> ~/.config/nix/nix.conf \
 && cat ~/.config/nix/nix.conf | grep 'trace' >/dev/null && /bin/true || echo 'show-trace = true' >> ~/.config/nix/nix.conf \
-&& test -d ~/.config/nixpkgs || mkdir --parent --mode=755 ~/.config/nixpkgs && touch ~/.config/nixpkgs/config.nix \
+&& test -d ~/.config/nixpkgs || mkdir --parent --mode=0755 ~/.config/nixpkgs && touch ~/.config/nixpkgs/config.nix \
 && cat ~/.config/nixpkgs/config.nix | grep 'allowUnfree' >/dev/null && /bin/true || echo '{ allowUnfree = true; }' >> ~/.config/nixpkgs/config.nix
 
 
