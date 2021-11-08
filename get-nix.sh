c8aa7814c5635f3666fa18cc87b147450f62a480
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-11-07 23:35:36 -0300

    One more

--- get-nix.sh
+++ get-nix.sh
@@ -41,10 +41,8 @@ test -d ~/.config/nix || mkdir --parent --mode=0755 ~/.config/nix && touch ~/.co
 && stat "$HOME"/.nix-profile/etc/profile.d/nix.sh \
 && sed -i 's|unset NIX_LINK|&\n'"${aux2}"'|' "$HOME"/.nix-profile/etc/profile.d/nix.sh \
 && echo \
-&& mv "$HOME"/.nix-profile/bin/nix "$HOME"/.nix-profile/bin/aux_nix \
-&& rm -fv "$HOME"/.nix-profile/bin/nix \
-&& mv "$HOME"/.nix-profile/bin/aux_nix "${aux1}" \
-&& mv "${aux1}"/aux_nix "${aux1}"/nix
+&& mv "$HOME"/.nix-profile/bin/nix "${aux1}"/nix \
+&& rm -fv "$HOME"/.nix-profile/bin/nix
 
 # Main idea from: https://stackoverflow.com/a/1167849
 NIX_HELPER_FUNCTIONS=$(cat <<-EOF
